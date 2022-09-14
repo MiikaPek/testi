6ec3e2a35f61b11a9c6a3808068e2de6205a9488
MiikaPekkarinen <94829050+MiikaPekkarinen@users.noreply.github.com>
2022-09-14 10:25:16 +0300

    serverin määrittely

---
+++ server.js
@@ -0,0 +1,22 @@
+// Ota express käyttöön
+const express = require("express");
+const app =express();
+
+// Ota mongoose käyttöön
+const mongoose = require("mongoose");
+
+// Ota MongoDB käyttöön
+const mongodb = require("mongodb");
+
+// Ota bodyparser käyttöön
+const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
+
+// Aseta määritykset express-palvelimelle
+// Ota käyttöön public-tiedosto
+app.use(express.static("public"));
+    // Ota käyttöön bodyparser
+app.use(bodyParser.urlencoded({extended:false}));
+
+// Laitetaan palvelin kuuntelemaan porttia 8080
+const server = app.listen(8080, function(){});
