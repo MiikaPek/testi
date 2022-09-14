488c209431fe3c1c8191f7295581cfe0a4691867
MiikaPekkarinen <94829050+MiikaPekkarinen@users.noreply.github.com>
2022-09-14 10:14:01 +0300

    npm pakettien asennus

---
+++ public/server.js
@@ -0,0 +1,10 @@
+// Ota express käyttöön
+const express = require("express");
+const app =express();
+
+// Ota mongoose käyttöön
+const mongoose = require("mongoose");
+
+// Ota bodyparser käyttöön
+const bodyparser = require("body-parser");
+
